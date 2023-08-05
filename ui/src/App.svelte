335eb0fdb0b93b2a21304bfb0e742ab2d76bdf75
sakshi-salokhe <sakshisalokhe1@gmail.com>
2023-08-05 07:50:13 -0500

    cleanup bits

--- ui/src/App.svelte
+++ ui/src/App.svelte
@@ -3,15 +3,16 @@
 	import { TEMP_HIDE, browserMode, resName } from '@store/stores'
 	import DebugBrowser from '@providers/DebugBrowser.svelte'
 	import AlwaysListener from '@providers/AlwaysListener.svelte'
-	import RealtorMenu from '@components/RealtorMenu.svelte'
+	// import RealtorMenu from '@components/RealtorMenu.svelte'
+	import RealtorMenu from '@components/newCodes/base/RealtorMenu.svelte'
 
 	$resName = 'Svelte-Template' // Change this to your resource name (case sensitive)
 </script>
 
 <VisibilityProvider>
 	<!-- PUT STUFF HERE  -->
-
 	<RealtorMenu />
+
 	{#if $TEMP_HIDE}
 		<div class="absolute top-1/2 left-4 h-fit bg-[color:var(--color-primary)] w-fit px-6 p-4 flex-col flex gap-4 items-center justify-center text-2xl font-bold">
 			<p>Press E to confirm</p>
@@ -20,15 +21,8 @@
 	{/if}
 </VisibilityProvider>
 
-
-
-<style>
-
-
-</style>
-
-
 <AlwaysListener />
+
 {#if $browserMode}
 	<DebugBrowser />
 {/if}
--- ui/src/components/RealtorMenu.svelte
+++ ui/src/components/RealtorMenu.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-	import Tabs from './Tabs.svelte'
+	import Tabs from '@components/Tabs.svelte'
 	import type { ITab } from '@typings/type'
-	import Properties from './Properties.svelte'
+	import Properties from '@components/Properties.svelte'
 	import { REALTOR_GRADE, TEMP_HIDE, CONFIG } from '@store/stores'
-	import Apartments from './Apartments.svelte'
+	import Apartments from '@components/Apartments.svelte'
 
 	let AvailableTabs: ITab[] = [
 		{
@@ -11,14 +11,7 @@
 			icon: 'fas fa-home',
 			component: Properties,
 		},
-
 	]
-
-	// {
-	// 		name: 'Apartments',
-	// 		icon: 'fas fa-building',
-	// 		component: Apartments,
-	// 	},
 
 	REALTOR_GRADE.subscribe((value) => {
 		if (value >= $CONFIG.setApartments) {
@@ -35,11 +28,10 @@
 
 <div
 	class="w-[60%] h-[90%] bg-[color:var(--color-primary)] absolute -translate-x-1/2 left-1/2 top-1/2 -translate-y-1/2 flex flex-col"
-	style="opacity: {$TEMP_HIDE?"0":"1"};"
+	style="opacity: {$TEMP_HIDE ? "0" : "1"};"
 >
-	<div class=" w-full h-full overflow-hidden">
+	<div class="w-full h-full overflow-hidden">
 		<svelte:component this={selectedTab.component} />
 	</div>
 	<Tabs bind:selectedTab {AvailableTabs} />
-
 </div>

a063bcbfa625266f32e5fdac48aa0029b30336da
sakshi-salokhe <sakshisalokhe1@gmail.com>
2023-08-06 08:26:52 -0500

    property card css

--- ui/src/components/properties/PropertyCard.svelte
+++ ui/src/components/properties/PropertyCard.svelte
@@ -1,3 +1,31 @@
 <div class="property-card-wrapper">
-here
+    <img src="/images/property-card-img.png" />
+
+    <div class="property-card-details">
+        <p class="property-name">Elgin Ave 2</p>
+        <p class="property-address">Legion Square</p>
+        <p class="property-cost">$ 10,000 USD</p>
+        <div class="property-details">
+            <div class="each-tile">
+                <i class="fas fa-dollar-sign"></i>
+                For Sale
+            </div>
+            <div class="each-tile">
+                <i class="fas fa-image"></i>
+                Gallery
+            </div>
+            <div class="each-tile">
+                <i class="fas fa-bath"></i>
+                Baths: 2
+            </div>
+            <div class="each-tile">
+                <i class="fas fa-house-chimney"></i>
+                House
+            </div>
+            <div class="each-tile">
+                <i class="fas fa-truck-front"></i>
+                Garage: Yes
+            </div>
+        </div>
+    </div>
 </div>

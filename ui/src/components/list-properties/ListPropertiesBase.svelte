0ceaf7d0c6945c9d1f5d8dac02b8e05eccb05fa9
sakshi-salokhe <sakshisalokhe1@gmail.com>
2023-08-06 16:02:51 -0500

    list new property form css

--- ui/src/components/list-properties/ListPropertiesBase.svelte
+++ ui/src/components/list-properties/ListPropertiesBase.svelte
@@ -1,3 +1,9 @@
+<script>
+	import DropdownComponent from "@components/generic/DropdownComponent.svelte"
+	import FormWrapperDropdown from "@components/generic/FormWrapperDropdown.svelte"
+import SetNotSetIndicator from "@components/generic/SetNotSetIndicator.svelte"
+
+</script>
 <!-- <div class="no-new-properties-base">
     <img src="/images/House.png" />
 
@@ -26,7 +32,47 @@
             <p class="info">Some instructions lorem lpsum is simply dummy text of printing</p>
         </div>
         <div class="right-column">
+            <div id="door-creation" class="form-row-wrapper">
+                <p class="label">Door Creation</p>
             
+                <div class="action-row">
+                    <SetNotSetIndicator leftValue="Door" rightValue="Not Set" good={false} />
+                    <button class="regular-button">Set Door</button>
+                </div>
+            </div>
+
+            <div id="garage-creation" class="form-row-wrapper">
+                <p class="label">Garage Creation</p>
+            
+                <div class="action-row">
+                    <SetNotSetIndicator leftValue="Garage" rightValue="Set" good={true} />
+                    <button class="regular-button">Set Garage</button>
+                </div>
+            </div>
+
+            <div id="description" class="form-row-wrapper">
+                <p class="label">Description</p>
+            
+                <div class="action-row">
+                    <textarea rows="5" placeholder="Write a short and sweet description about yourself..." />
+                </div>
+            </div>
+
+            <div id="price" class="form-row-wrapper">
+                <p class="label">Price</p>
+            
+                <div class="action-row">
+                    <input type="text" placeholder="$1000000000" />
+                </div>
+            </div>
+
+            <div id="shell-type" class="form-row-wrapper">
+                <p class="label">Shell Type</p>
+            
+                <div class="action-row">
+                    <FormWrapperDropdown dropdownValues={[]} label="" id="new-listing-dd-shell-type" selectedValue="House" insideLabel="Type: " />
+                </div>
+            </div>
         </div>
     </div>
 </div>

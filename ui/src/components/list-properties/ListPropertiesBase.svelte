6130dd11b2f5355982e1135036b67b3531e5ea78
sakshi-salokhe <sakshisalokhe1@gmail.com>
2023-08-06 15:08:33 -0500

    list new property form basic css layout

--- ui/src/components/list-properties/ListPropertiesBase.svelte
+++ ui/src/components/list-properties/ListPropertiesBase.svelte
@@ -1,4 +1,4 @@
-<div class="no-new-properties-base">
+<!-- <div class="no-new-properties-base">
     <img src="/images/House.png" />
 
     <p>You haven't listed a property yet!</p>
@@ -6,4 +6,27 @@
     <button>
         Add New Property
     </button>
+</div> -->
+
+<div class="list-new-property-form">
+    <div class="header">
+        <div class="heading-title-wrapper">
+            <i class="fas fa-circle-plus add-icon"></i>
+            <p>List New Property</p>
+        </div>
+        <div>
+            <i class="fas fa-chevron-down chevron-icon"></i>
+        </div>
+    </div>
+
+    <div class="body-wrapper">
+        <div class="left-column">
+            <p class="title">Property Information</p>
+
+            <p class="info">Some instructions lorem lpsum is simply dummy text of printing</p>
+        </div>
+        <div class="right-column">
+            
+        </div>
+    </div>
 </div>

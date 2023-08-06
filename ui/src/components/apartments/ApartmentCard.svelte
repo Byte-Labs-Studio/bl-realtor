8a6e70f1d4eb1fb8d846ddfd7752f461ac5ca292
sakshi-salokhe <sakshisalokhe1@gmail.com>
2023-08-06 14:38:39 -0500

    apt page css done

--- ui/src/components/apartments/ApartmentCard.svelte
+++ ui/src/components/apartments/ApartmentCard.svelte
@@ -1,3 +1,18 @@
 <div class="apartment-card-wrapper">
+    <img src="/images/apt-card-img.png" />
 
+    <div class="details">
+        <p class="heading">Elgin Ave 2</p>
+
+        <p class="info">Legion Square</p>
+
+        <div class="location-tenants-info">
+            <img src="/images/user-location-pin.png" />
+            <p>100 / 150 Global tenants</p>
+        </div>
+
+        <button class="select-apt-button">
+            Select Apartment
+        </button>
+    </div>
 </div>
